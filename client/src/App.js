8ca72553c07763bd25f3b3dcf0198add79733660
Nishita Pande <pande.nishita@gmail.com>
2023-04-24 12:22:58 +0530

    first commit

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import NavBar from "./commponents/NavBar";
+// import NavBar from "./components/NavBar";
+import Home from "./pages/Home";
+//import Login from "./components/Login";
+import CreatePost from "./pages/CreatePostPage.js";
+import Blogs from "./pages/Blogs";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { Auth0Provider } from "@auth0/auth0-react";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Auth0Provider
+      domain="dev-7ougw7dd26p4l8i3.us.auth0.com"
+      clientId="xyPaWIk3l8Gs8zfeK4jpEtZsYobrKR1j"
+      authorizationParams={{
+        redirect_uri: window.location.origin,
+      }}
+    >
+      <Router>
+        <div className="App">
+          <NavBar />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            {/* <Route path="/login" element={<Login />} /> */}
+            <Route path="/writeblog" element={<CreatePost />} />
+            <Route path="/blogs" element={<Blogs />} />
+          </Routes>
+        </div>
+      </Router>
+    </Auth0Provider>
   );
 }
 
---
+++ client/src/commponents/NavBar.js
@@ -0,0 +1,108 @@
+import React, { useState } from "react";
+
+import { useAuth0 } from "@auth0/auth0-react";
+
+function NavBar() {
+  const [openResouceDropdown, setOpenResouceDropdown] = useState(false);
+  const { isAuthenticated } = useAuth0();
+  return (
+    <div className="nav-bar-section">
+      <div className="nav-bar">
+        <a href="#" className="logo">
+          {" "}
+          Logo
+          {/* <limg src={img2} /> */}
+        </a>
+        <div className="links-box">
+          <Link className="nav-bar-list-links" to="/">
+            Home
+          </Link>
+          <Link className="nav-bar-list-links" to="/about">
+            About
+          </Link>
+          <Link
+            className="nav-bar-list-links"
+            to="/resources"
+            onClick={() => setOpenResouceDropdown((prev) => !prev)}
+          >
+            Resources
+            {openResouceDropdown && <DropdownResources />}
+          </Link>
+
+          <Link className="nav-bar-list-links" to="/blogs">
+            Blogs
+          </Link>
+          {/* <Link className="cta" to="/try-now">
+        LOG IN
+      </Link> */}
+          {isAuthenticated ? <Profile /> : <LoginButton />}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+const DropdownResources = () => {
+  return (
+    <div className="drop-down-resource">
+      <ul className="drop-down">
+        <li className="drop-down-list">
+          {" "}
+          <a className="drop-down-link"> PDFS</a>{" "}
+        </li>
+        <li className="drop-down-list">
+          {" "}
+          <a className="drop-down-link">Video</a>{" "}
+        </li>
+        <li className="drop-down-list">
+          {" "}
+          <a className="drop-down-link">Class</a>{" "}
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+const Profile = () => {
+  const [openProfile, setOpenProfile] = useState(false);
+  return (
+    <div className="drop-down-profile">
+      <img src={img1} onClick={() => setOpenProfile((prev) => !prev)} />
+      {openProfile && <DropdownProfile />}
+    </div>
+  );
+};
+
+const DropdownProfile = () => {
+  return (
+    <div className="dropdown-profile">
+      <ul className="drop-down">
+        <li className="drop-down-list">
+          {" "}
+          <a className="drop-down-link" to="/editprofile">
+            {" "}
+            Edit Profile{" "}
+          </a>{" "}
+        </li>
+        <li className="drop-down-list">
+          {" "}
+          <a className="drop-down-link">My Learning </a>{" "}
+        </li>
+        <li className="drop-down-list">
+          {" "}
+          <a className="drop-down-link" href="/writeblog">
+            {" "}
+            Write Blog
+          </a>{" "}
+        </li>
+        <li className="drop-down-list">
+          {" "}
+          <a className="drop-down-link">Saved</a>{" "}
+        </li>
+        <LogoutButton />
+      </ul>
+    </div>
+  );
+};
+
+export default NavBar;
